edbe81ca1d42677c4fd056db5e293e0c3a910f6b
Vieru Andrei <mixerandr3w001@gmail.com>
2023-02-27 17:56:30 +0200

    Adaug stergere spatii multiple

--- Tema1/p2.py
+++ Tema1/p2.py
@@ -1,6 +1,13 @@
-fisier = open("data.txt", "r")
+from pathlib import Path
+import re
+
+dirname = str(Path(__file__).parent.absolute())
+
+fisier = open(dirname + "/data.txt", "r")
 
 continut = fisier.read()
+
+fisier.close()
 
 vreau_sa_dispar = [
     ",",
@@ -23,5 +30,7 @@ vreau_sa_dispar = [
 
 for ch in vreau_sa_dispar:
     continut = continut.replace(ch, "")
+
+continut = re.sub(" +", " ", continut)
 
 print(continut)
